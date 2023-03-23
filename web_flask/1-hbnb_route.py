287fb66c561178760bcb774048335cb9e0c3f61f
Paul Ibikunle <42482454+Ibkpurl@users.noreply.github.com>
2023-03-23 19:38:07 +0100

    AirBnB clone - MySQL

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def display_hello():
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def desplay_hbnb():
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application"""
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
 from flask import Flask
-from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def display_hello():
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def desplay_hbnb():
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def display_cText(text):
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
-    return "C %s" % (text)
+    return "C {}".format(text)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
